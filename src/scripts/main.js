10349fe7141bbd1b82ed31e968acdf4851f0f7ce
tatomyr <andrew.tatomyr@gmail.com>
2017-07-23 20:53:17 +0300

    fetched data from opendrive

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,97 @@
 // Animations
-var os = new OnScreen();
+const effectsIn = [
+  'bounce',
+  'flash',
+  'pulse',
+  'rubberBand',
+  'shake',
+  'headShake',
+  'tada',
+  'wobble',
+  'jello',
+  'bounceIn',
+  'bounceInDown',
+  'bounceInLeft',
+  'bounceInRight',
+  'bounceInUp',
+  'fadeIn',
+  'fadeInDown',
+  'fadeInDownBig',
+  'fadeInLeft',
+  'fadeInLeftBig',
+  'fadeInRight',
+  'fadeInRightBig',
+  'fadeInUp',
+  'fadeInUpBig',
+  'flipInX',
+  'flipInY',
+  'lightSpeedIn',
+  'rotateIn',
+  'rotateInDownLeft',
+  'rotateInDownRight',
+  'rotateInUpLeft',
+  'rotateInUpRight',
+  'jackInTheBox',
+  'rollIn',
+  'zoomIn',
+  'zoomInDown',
+  'zoomInLeft',
+  'zoomInRight',
+  'zoomInUp',
+  'slideInDown',
+  'slideInLeft',
+  'slideInRight',
+  'slideInUp',
+];
 
-os.on('enter', '.greeting', (element, event) => {
+const effectsOut = [
+  'swing',
+  'bounceOut',
+  'bounceOutDown',
+  'bounceOutLeft',
+  'bounceOutRight',
+  'bounceOutUp',
+  'fadeOut',
+  'fadeOutDown',
+  'fadeOutDownBig',
+  'fadeOutLeft',
+  'fadeOutLeftBig',
+  'fadeOutRight',
+  'fadeOutRightBig',
+  'fadeOutUp',
+  'fadeOutUpBig',
+  'flipOutX',
+  'flipOutY',
+  'lightSpeedOut',
+  'rotateOut',
+  'rotateOutDownLeft',
+  'rotateOutDownRight',
+  'rotateOutUpLeft',
+  'rotateOutUpRight',
+  'hinge',
+  'rollOut',
+  'zoomOut',
+  'zoomOutDown',
+  'zoomOutLeft',
+  'zoomOutRight',
+  'zoomOutUp',
+  'slideOutDown',
+  'slideOutLeft',
+  'slideOutRight',
+  'slideOutUp',
+];
+
+const effects = [
+  ...effectsIn,
+  // ...effectsOut
+];
+
+const os = new OnScreen();
+
+os.on('enter', '.animated', (element, event) => {
   setTimeout(() => {
-    $(element).addClass('animated bounce');
+    const rnd = Math.round(Math.random() * effects.length);
+    $(element).removeClass();
+    $(element).addClass('animated ' + effects[rnd]);
   }, element.dataset.debounce || 0);
 });
