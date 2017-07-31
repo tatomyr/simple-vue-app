1174e1fda4911553494ac2e8ae2b21b9a22f0304
tatomyr <andrew.tatomyr@gmail.com>
2017-07-31 19:25:40 +0300

    added restdb.io

--- src/scripts/vm.js
+++ src/scripts/vm.js
@@ -9,6 +9,7 @@ const vm = new Vue({
 
   },
   created: function () {
+    /* *
     // Fetch opendrive folder
     fetch('https://dev.opendrive.com/api/v1/folder/shared.json/NDdfNTEwNjEwXw?order_type=asc').then(res => res.json()).then(doc => {
       console.log(doc.Files.find(item => item.Name === 'text.txt'));
@@ -22,5 +23,51 @@ const vm = new Vue({
         this.text = doc.trim().split('\n');
       });
     });
+    /* */
+
+    // Interact with restdb.io
+    fetch('https://cmstest-b1db.restdb.io/rest/content', {
+      "headers": {
+        "content-type": "application/json",
+        "x-apikey": "597f3006fc7b2b2169cafd8a",
+        "cache-control": "no-cache"
+      }
+    }).then(res => res.json()).then(doc => {
+      console.log(doc[0]);
+      this.text = doc[0].list
+
+    });
+
   },
 });
+
+
+
+/*
+var jsondata = {
+"title": "Simple Vue App",
+"greeting": "Hello Vue",
+"list": [
+    "name",
+    "image",
+    "description",
+    "topic"
+    ]
+};
+var settings = {
+  "async": true,
+  "crossDomain": true,
+  "url": "https://cmstest-b1db.restdb.io/rest/content",
+  "method": "POST",
+  "headers": {
+    "content-type": "application/json",
+    "x-apikey": "597f3022fc7b2b2169cafd8b",
+    "cache-control": "no-cache"
+  },
+  "processData": false,
+  "data": JSON.stringify(jsondata)
+}
+$.ajax(settings).done(function (response) {
+  console.log(response);
+});
+*/
